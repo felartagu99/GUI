a35f2a4606e4130016d7c395c3eac53c158aa3dd
felartagu99 <felipeaa99@gmail.com>
2024-05-31 19:58:55 +0200

    Gitignore

--- main.py
+++ main.py
@@ -452,7 +452,6 @@ class CameraApp(QWidget):
         self.dataset_generation_interface.show()
         self.close()
 
-
     def update_format_combo(self):
             self.format_combo.clear()
             if self.sam_button.isChecked():
@@ -469,10 +468,10 @@ class CameraApp(QWidget):
  
     def load_models(self):
         # Cargar modelo GroundingDino
-        self.groundingdino_model = load_model("./GUI/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "./GUI/checkpoints/groundingdino_swint_ogc.pth")
+        self.groundingdino_model = load_model("./GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "./checkpoints/groundingdino_swint_ogc.pth")
 
         # Cargar modelo SAM
-        sam_checkpoint = "./GUI/checkpoints/sam_vit_h_4b8939.pth"
+        sam_checkpoint = "./checkpoints/sam_vit_h_4b8939.pth"
         model_type = "vit_h"
         self.sam_model = sam_model_registry[model_type](checkpoint=sam_checkpoint)
         self.sam_predictor = SamPredictor(self.sam_model)
