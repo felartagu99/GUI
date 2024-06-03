71b3a64caf26755780d115726ceaddaa17670dba
manuelzamoraperez01 <130736912+manuelzamoraperez01@users.noreply.github.com>
2024-06-03 19:07:07 +0200

    export

--- dataset_generation_interface.py
+++ dataset_generation_interface.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QHBoxLayout, QComboBox, QMessageBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QHBoxLayout, QComboBox, QMessageBox, QDialog, QDialogButtonBox
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 import os
@@ -41,7 +41,7 @@ class DatasetGenerationInterface(QWidget):
         layout.addLayout(percentage_layout)
 
         # Botón para dividir dataset
-        self.split_dataset_button = self.create_button("Dividir Dataset", "#3498db", "#2980b9", self.split_dataset)
+        self.split_dataset_button = self.create_button("Dividir Dataset", "#3498db", "#2980b9", self.show_format_selection_popup)
         self.split_dataset_button.setEnabled(False)
         layout.addWidget(self.split_dataset_button, alignment=Qt.AlignCenter)
 
@@ -70,6 +70,11 @@ class DatasetGenerationInterface(QWidget):
             combo.addItem(f"{i}%")
         return combo
 
+    def split_dataset(self, split_function, dialog):
+        dialog.accept()
+        split_function()
+
+
     def select_dataset(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -82,8 +87,52 @@ class DatasetGenerationInterface(QWidget):
             self.status_label.setText(f"Dataset seleccionado: {self.dataset_dir}")
             self.split_dataset_button.setEnabled(True)
 
+    '''
+    def copy_files(self, pairs, img_dest_dir, ann_dest_dir=None):
+        for img, ann_files in pairs:
+            shutil.copy(img, os.path.join(img_dest_dir, os.path.basename(img)))
+            print(f"Copied {img} to {img_dest_dir}")  # Mensaje de depuración
+            if ann_dest_dir:
+                for ann in ann_files:
+                    shutil.copy(ann, os.path.join(ann_dest_dir, os.path.basename(ann)))
+                    print(f"Copied {ann} to {ann_dest_dir}")  # Mensaje de depuración
+                    '''
 
-    def split_dataset(self):
+    def copy_files_2(self, pairs, images_dir, annotations_dir):
+        for image_path, annotation_paths in pairs:
+            print(f"Copying image: {image_path} to {images_dir}")  # Depuración
+            shutil.copy(image_path, os.path.join(images_dir, os.path.basename(image_path)))
+            for annotation_path in annotation_paths:
+                print(f"Copying annotation: {annotation_path} to {annotations_dir}")  # Depuración
+                shutil.copy(annotation_path, os.path.join(annotations_dir, os.path.basename(annotation_path)))
+                
+
+    def show_format_selection_popup(self):
+        popup = QDialog(self)
+        popup.setWindowTitle("Seleccionar Formato")
+        layout = QVBoxLayout()
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(popup.accept)
+        button_box.rejected.connect(popup.reject)
+
+        layout.addWidget(QLabel("Elige uno de estos 4 formatos:"))
+        formats = [("Yolo", self.split_dataset_yolo), 
+                   ("Coco", self.split_dataset_coco), 
+                   ("Pascal-Voc", self.split_dataset_pascal_voc), 
+                   ("Segmentación", self.split_dataset_segmentation)]
+
+        for format_name, format_func in formats:
+            button = QPushButton(format_name)
+            button.clicked.connect(lambda _, func=format_func: self.split_dataset(format_func, popup))
+            layout.addWidget(button)
+
+        layout.addWidget(button_box)
+        popup.setLayout(layout)
+        popup.exec_()
+    
+    
+    def split_dataset_pascal_voc(self):
         if self.dataset_dir:
             # Obtener los porcentajes seleccionados
             train_percentage = int(self.train_percentage_combo.currentText().strip('%')) / 100
@@ -163,6 +212,259 @@ class DatasetGenerationInterface(QWidget):
 
 
 
+
+    def split_dataset_yolo(self):
+        if self.dataset_dir:
+            # Obtener los porcentajes seleccionados
+            train_percentage = int(self.train_percentage_combo.currentText().strip('%')) / 100
+            valid_percentage = int(self.valid_percentage_combo.currentText().strip('%')) / 100
+            test_percentage = int(self.test_percentage_combo.currentText().strip('%')) / 100
+
+            if train_percentage + valid_percentage + test_percentage != 1.0:
+                self.status_label.setText("Los porcentajes deben sumar 100%.")
+                return
+
+            # Directorios para guardar las divisiones del dataset
+            split_dir = os.path.join(self.dataset_dir, 'split_dataset')
+            train_dir = os.path.join(split_dir, 'train')
+            valid_dir = os.path.join(split_dir, 'valid')
+            test_dir = os.path.join(split_dir, 'test')
+
+            os.makedirs(train_dir, exist_ok=True)
+            os.makedirs(valid_dir, exist_ok=True)
+            os.makedirs(test_dir, exist_ok=True)
+
+            # Subcarpetas para imágenes y anotaciones
+            train_images_dir = os.path.join(train_dir, 'images')
+            train_annotations_dir = os.path.join(train_dir, 'annotations')
+            valid_images_dir = os.path.join(valid_dir, 'images')
+            valid_annotations_dir = os.path.join(valid_dir, 'annotations')
+            test_images_dir = os.path.join(test_dir, 'images')
+            test_annotations_dir = os.path.join(test_dir, 'annotations')
+
+            os.makedirs(train_images_dir, exist_ok=True)
+            os.makedirs(train_annotations_dir, exist_ok=True)
+            os.makedirs(valid_images_dir, exist_ok=True)
+            os.makedirs(valid_annotations_dir, exist_ok=True)
+            os.makedirs(test_images_dir, exist_ok=True)
+            os.makedirs(test_annotations_dir, exist_ok=True)
+
+            # Directorios de imágenes y anotaciones
+            images_dir = os.path.join(self.dataset_dir, 'images')
+            annotations_dir = os.path.join(self.dataset_dir, 'annotations')
+
+            images = []
+            annotations = {}
+
+            # Recopilar archivos de imágenes
+            for root, _, filenames in os.walk(images_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+                        base_name = os.path.splitext(file)[0]
+                        images.append(os.path.join(root, file))
+                        annotations[base_name] = []
+
+            # Recopilar archivos de anotaciones
+            for root, _, filenames in os.walk(annotations_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.txt', '.xml', '.json')):
+                        base_name = os.path.splitext(file)[0]
+                        if base_name in annotations:
+                            annotations[base_name].append(os.path.join(root, file))
+                        else:
+                            annotations[base_name] = [os.path.join(root, file)]
+
+            # Emparejar las imágenes con sus anotaciones correspondientes
+            paired_files = [(img, annotations.get(os.path.splitext(os.path.basename(img))[0], [])) for img in images]
+
+            # Filtrar solo los pares completos (con al menos una anotación)
+            paired_files = [pair for pair in paired_files if pair[1]]
+
+            print(f"Total pairs found: {len(paired_files)}")  # Mensaje de depuración
+
+            random.shuffle(paired_files)
+            train_pairs = paired_files[:int(train_percentage * len(paired_files))]
+            valid_pairs = paired_files[int(train_percentage * len(paired_files)):int((train_percentage + valid_percentage) * len(paired_files))]
+            test_pairs = paired_files[int((train_percentage + valid_percentage) * len(paired_files)):]
+
+            print(f"Train pairs: {len(train_pairs)}")  # Mensaje de depuración
+            print(f"Valid pairs: {len(valid_pairs)}")  # Mensaje de depuración
+            print(f"Test pairs: {len(test_pairs)}")  # Mensaje de depuración
+
+            def copy_files(pairs, img_dest_dir, ann_dest_dir):
+                for img, ann_files in pairs:
+                    shutil.copy(img, os.path.join(img_dest_dir, os.path.basename(img)))
+                    print(f"Copied {img} to {img_dest_dir}")  # Mensaje de depuración
+                    for ann in ann_files:
+                        shutil.copy(ann, os.path.join(ann_dest_dir, os.path.basename(ann)))
+                        print(f"Copied {ann} to {ann_dest_dir}")  # Mensaje de depuración
+
+            copy_files(train_pairs, train_images_dir, train_annotations_dir)
+            copy_files(valid_pairs, valid_images_dir, valid_annotations_dir)
+            copy_files(test_pairs, test_images_dir, test_annotations_dir)
+
+            self.show_popup("Dataset dividido en carpetas 'train', 'valid' y 'test' con subcarpetas 'images' y 'annotations'.")
+            self.status_label.setText("Dataset dividido en carpetas 'train', 'valid' y 'test' con subcarpetas 'images' y 'annotations'.")
+             
+    
+    def split_dataset_coco(self):
+         if self.dataset_dir:
+            # Obtener los porcentajes seleccionados
+            train_percentage = int(self.train_percentage_combo.currentText().strip('%')) / 100
+            valid_percentage = int(self.valid_percentage_combo.currentText().strip('%')) / 100
+            test_percentage = int(self.test_percentage_combo.currentText().strip('%')) / 100
+
+            if train_percentage + valid_percentage + test_percentage != 1.0:
+                self.status_label.setText("Los porcentajes deben sumar 100%.")
+                return
+
+            # Directorios para guardar las divisiones del dataset
+            split_dir = os.path.join(self.dataset_dir, 'split_dataset')
+            train_dir = os.path.join(split_dir, 'train')
+            valid_dir = os.path.join(split_dir, 'valid')
+            test_dir = os.path.join(split_dir, 'test')
+
+            os.makedirs(train_dir, exist_ok=True)
+            os.makedirs(valid_dir, exist_ok=True)
+            os.makedirs(test_dir, exist_ok=True)
+
+            # Directorios de imágenes y anotaciones
+            images_dir = os.path.join(self.dataset_dir, 'images')
+            annotations_dir = os.path.join(self.dataset_dir, 'annotations')
+
+            images = []
+            annotations = {}
+
+            # Recopilar archivos de imágenes
+            for root, _, filenames in os.walk(images_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+                        base_name = os.path.splitext(file)[0]
+                        images.append(os.path.join(root, file))
+                        annotations[base_name] = []
+
+            # Recopilar archivos de anotaciones
+            for root, _, filenames in os.walk(annotations_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.txt', '.xml', '.json')):
+                        base_name = os.path.splitext(file)[0]
+                        if base_name in annotations:
+                            annotations[base_name].append(os.path.join(root, file))
+                        else:
+                            annotations[base_name] = [os.path.join(root, file)]
+
+            # Emparejar las imágenes con sus anotaciones correspondientes
+            paired_files = [(img, annotations.get(os.path.splitext(os.path.basename(img))[0], [])) for img in images]
+
+            # Filtrar solo los pares completos (con al menos una anotación)
+            paired_files = [pair for pair in paired_files if pair[1]]
+
+            print(f"Total pairs found: {len(paired_files)}")  # Mensaje de depuración
+
+            random.shuffle(paired_files)
+            train_pairs = paired_files[:int(train_percentage * len(paired_files))]
+            valid_pairs = paired_files[int(train_percentage * len(paired_files)):int((train_percentage + valid_percentage) * len(paired_files))]
+            test_pairs = paired_files[int((train_percentage + valid_percentage) * len(paired_files)):]
+
+            print(f"Train pairs: {len(train_pairs)}")  # Mensaje de depuración
+            print(f"Valid pairs: {len(valid_pairs)}")  # Mensaje de depuración
+            print(f"Test pairs: {len(test_pairs)}")  # Mensaje de depuración
+
+            def copy_files(pairs, dest_dir):
+                for img, ann_files in pairs:
+                    shutil.copy(img, os.path.join(dest_dir, os.path.basename(img)))
+                    print(f"Copied {img} to {dest_dir}")  # Mensaje de depuración
+                    for ann in ann_files:
+                        shutil.copy(ann, os.path.join(dest_dir, os.path.basename(ann)))
+                        print(f"Copied {ann} to {dest_dir}")  # Mensaje de depuración
+
+            copy_files(train_pairs, train_dir)
+            copy_files(valid_pairs, valid_dir)
+            copy_files(test_pairs, test_dir)
+
+            self.show_popup("Dataset dividido en carpetas 'train', 'valid' y 'test'.")
+            self.status_label.setText("Dataset dividido en carpetas 'train', 'valid' y 'test'.")   
+
+    def split_dataset_segmentation(self):
+         if self.dataset_dir:
+            # Obtener los porcentajes seleccionados
+            train_percentage = int(self.train_percentage_combo.currentText().strip('%')) / 100
+            valid_percentage = int(self.valid_percentage_combo.currentText().strip('%')) / 100
+            test_percentage = int(self.test_percentage_combo.currentText().strip('%')) / 100
+
+            if train_percentage + valid_percentage + test_percentage != 1.0:
+                self.status_label.setText("Los porcentajes deben sumar 100%.")
+                return
+
+            # Directorios para guardar las divisiones del dataset
+            split_dir = os.path.join(self.dataset_dir, 'split_dataset')
+            train_dir = os.path.join(split_dir, 'train')
+            valid_dir = os.path.join(split_dir, 'valid')
+            test_dir = os.path.join(split_dir, 'test')
+
+            os.makedirs(train_dir, exist_ok=True)
+            os.makedirs(valid_dir, exist_ok=True)
+            os.makedirs(test_dir, exist_ok=True)
+
+            # Directorios de imágenes y anotaciones
+            images_dir = os.path.join(self.dataset_dir, 'images')
+            annotations_dir = os.path.join(self.dataset_dir, 'annotations')
+
+            images = []
+            annotations = {}
+
+            # Recopilar archivos de imágenes
+            for root, _, filenames in os.walk(images_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+                        base_name = os.path.splitext(file)[0]
+                        images.append(os.path.join(root, file))
+                        annotations[base_name] = []
+
+            # Recopilar archivos de anotaciones
+            for root, _, filenames in os.walk(annotations_dir):
+                for file in filenames:
+                    if file.lower().endswith(('.txt', '.xml', '.json')):
+                        base_name = os.path.splitext(file)[0]
+                        if base_name in annotations:
+                            annotations[base_name].append(os.path.join(root, file))
+                        else:
+                            annotations[base_name] = [os.path.join(root, file)]
+
+            # Emparejar las imágenes con sus anotaciones correspondientes
+            paired_files = [(img, annotations.get(os.path.splitext(os.path.basename(img))[0], [])) for img in images]
+
+            # Filtrar solo los pares completos (con al menos una anotación)
+            paired_files = [pair for pair in paired_files if pair[1]]
+
+            print(f"Total pairs found: {len(paired_files)}")  # Mensaje de depuración
+
+            random.shuffle(paired_files)
+            train_pairs = paired_files[:int(train_percentage * len(paired_files))]
+            valid_pairs = paired_files[int(train_percentage * len(paired_files)):int((train_percentage + valid_percentage) * len(paired_files))]
+            test_pairs = paired_files[int((train_percentage + valid_percentage) * len(paired_files)):]
+
+            print(f"Train pairs: {len(train_pairs)}")  # Mensaje de depuración
+            print(f"Valid pairs: {len(valid_pairs)}")  # Mensaje de depuración
+            print(f"Test pairs: {len(test_pairs)}")  # Mensaje de depuración
+
+            def copy_files(pairs, dest_dir):
+                for img, ann_files in pairs:
+                    shutil.copy(img, os.path.join(dest_dir, os.path.basename(img)))
+                    print(f"Copied {img} to {dest_dir}")  # Mensaje de depuración
+                    for ann in ann_files:
+                        shutil.copy(ann, os.path.join(dest_dir, os.path.basename(ann)))
+                        print(f"Copied {ann} to {dest_dir}")  # Mensaje de depuración
+
+            copy_files(train_pairs, train_dir)
+            copy_files(valid_pairs, valid_dir)
+            copy_files(test_pairs, test_dir)
+
+            self.show_popup("Dataset dividido en carpetas 'train', 'valid' y 'test'.")
+            self.status_label.setText("Dataset dividido en carpetas 'train', 'valid' y 'test'.")   
+             
+             
+    
 
     def show_popup(self, message):
         msg = QMessageBox()
